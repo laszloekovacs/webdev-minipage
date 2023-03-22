ce8756e5081e807ba8522974de137b54df7820a4
laszloekovacs <laszloekovacs@gmail.com>
2023-03-22 14:19:45 +0100

    fixed linking

--- src/components/LinkList.jsx
+++ src/components/LinkList.jsx
@@ -4,11 +4,12 @@ import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
 
 const queryClient = new QueryClient()
 
-const LinkListConsumer = () => {
-  const documenturl = 'http://localhost:3001/README.md'
+const documentUrl =
+  'https://raw.githubusercontent.com/laszloekovacs/webdev-minipage/master/README.md'
 
+const LinkListConsumer = () => {
   const { isLoading, error, data } = useQuery('links', () => {
-    const bytes = fetch('http://localhost:3001/links').then((res) => res.json())
+    const bytes = fetch(documentUrl).then((res) => res.json())
 
     /* transform the loaded md file into json */
     const md = new MarkdownIt()
