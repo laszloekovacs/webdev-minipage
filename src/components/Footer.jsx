93af7cad39d13a98bbfde42397e4bd3721b9783c
laszloekovacs <laszloekovacs@gmail.com>
2023-03-22 17:54:52 +0100

    added some fonts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <footer>
       <h2>LOGO</h2>
-      <p>© 2023 laszlo kovacs</p>
-      <p>made with some of the tools listed</p>
+      <p>2023 - Laszlo Kovacs</p>
+      <p>Made with some of the tools listed</p>
       <a href="http://github.com/laszloekovacs"></a>
     </footer>
   )
